2fe3801273a566551786dab04caf8caac1fd5132
Dharshan+2-at-988279413178 <cdharshans@gmail.com>
2019-09-03 23:59:17 +0530

    send email

--- routes/web.php
+++ routes/web.php
@@ -15,4 +15,4 @@ Route::get('/', function () {
     return view('welcome');
 });
 Route::get('/send/email', 'HomeController@mail');
-Route::post('/send/email', 'HomeController@sendEmail');
+//Route::post('/send/email', 'HomeController@sendEmail');
